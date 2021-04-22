76ffccc9e9f9082843780027226008e1d811fd04
Mohan Joshi <mohanj098@gmail.com>
2021-04-22 12:54:09 +0530

    route not found

--- front-end/src/Components/RouteHandler/index.js
+++ front-end/src/Components/RouteHandler/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom'
-import PrePage from '../PredictionPage/PrePage';
+import { Switch, Route, Redirect } from 'react-router-dom'
+import PrePage from '../PredictionPage/index';
 import Login from "../LogIn/Login";
 import Signup from "../SignUp/SignUp"
 import MainPage from '../MainPage/index';
@@ -8,9 +8,11 @@ import MainPage from '../MainPage/index';
 const index = () => {
     return (
         <Switch>
+            <Route path='/predict' exact component={PrePage}/>
             <Route path="/" exact component={MainPage} />
             <Route path="/Login" component={Login}/>
             <Route path="/Signup" component={Signup}/>
+            <Route path="*" render={()=>{return(<Redirect push to='/login'/>)}}/>
         </Switch>
     );
 }
