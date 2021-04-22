632451a1e286602ac638b6c4881ff65a48ada5f4
Daksh Sharma <68499451+2018csb1082@users.noreply.github.com>
2021-04-22 10:46:10 +0530

    done prediction page
    prediction oage fianlly edited

--- front-end/src/Components/PredictionPage/index.js
+++ front-end/src/Components/PredictionPage/index.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import PageLayout from '../PageLayout/PageLayout';
+import React, { useState } from "react";
+import { Tabs, Button, Divider, Checkbox } from 'antd';
+import logo from '../../common/images/logo_cropped.png';
+import logout from '../../common/images/logout.svg';
+import {LogoutOutlined} from '@ant-design/icons';
+import AddProduct from '../AddProduct/AddProduct';
 import PrePage from './PrePage';
 
-const index = (props) => {
+const { TabPane } = Tabs;
+
+const OperationsSlot = {
+    left: <div><img src={logo} /></div>,
+    right: <div><Button icon={<LogoutOutlined />} size="medium" type="primary" shape="round" className="button-LOGOUT">LOGOUT</Button></div>,
+};
+
+const index = () => {
+
     return (
-        <PageLayout {...props}>
-            <PrePage />
-        </PageLayout>
+        <div className="tab-container">
+            <Tabs size='large' tabBarExtraContent={OperationsSlot} tabBarStyle={{color: '#379683',fontWeight: '700',}}>
+                <TabPane tab="MY WISHLIST" key="1" style={{marginLeft: '20px'}}>
+                    <PrePage />
+                </TabPane>
+                <TabPane tab="ADD PRODUCT" disabled key="2">
+                    <AddProduct />
+                </TabPane>
+                <TabPane tab="ABOUT US" disabled key="3">
+                    Content of tab 3
+                </TabPane>
+            </Tabs>
+        </div>
     );
 }
 
